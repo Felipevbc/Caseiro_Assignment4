d13c09d8b4af79a3d6b164014c14d864984d6fb8
Felipevbc <60027336+Felipevbc@users.noreply.github.com>
2020-03-29 19:04:57 -0400

    Update Application.java

--- Caseiro_Problem2/src/Application.java
+++ Caseiro_Problem2/src/Application.java
@@ -1,1 +1,12 @@
-
+public class Application{
+   
+	public static void main(String[] args){
+     
+       DuplicateCounter Counter = new DuplicateCounter();
+       
+       Counter.count("problem2.txt");
+       
+       Counter.write("unique_word_counts.txt");
+      
+   }
+}
