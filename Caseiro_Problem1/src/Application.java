707599c94404435c54e6e8a32f9936659c126af3
Felipevbc <60027336+Felipevbc@users.noreply.github.com>
2020-03-29 18:56:44 -0400

    Update Application.java

--- Caseiro_Problem1/src/Application.java
+++ Caseiro_Problem1/src/Application.java
@@ -1,1 +1,13 @@
+import java.io.IOException;
 
+public class Application {
+
+   public static void main(String[] args) throws IOException {
+       
+      DuplicateRemover Remover = new DuplicateRemover();
+     
+      Remover.remove("problem1.txt");
+       
+      Remover.write("unique_words.txt");
+   }
+}
